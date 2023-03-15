798824941d94308420175f9cc2106279ac5a0eb3
HimashaNethmini <himashanethmini05@gmail.com>
2023-03-15 21:08:51 +0530

    Make Redux Features

---
+++ client/src/redux/features/userSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+export const userSlice = createSlice({
+    name: "User",
+    initialState: {
+        user:null,
+        listFavourites: []
+    },
+    reducers: {
+        setUser: (state, action) => {
+            if(action.payload == null) {
+                localStorage.removeItem("actkn")
+            }else {
+                if (action.payload.token) localStorage.setItem("actkn",action.payload.token)  
+            }
+
+            state.user = action.payload
+        },
+        setListFavourites: (state, action) =>{
+            state.listFavourites = action.payload
+        },
+        removeFavourites: (state, action) => {
+            const{ mediaId } = action.payload
+            state.listFavourites = [...state.listFavourites].filter(e => e.mediaId.toString() !== mediaId.toString());
+        },
+        addFavourites: (state, action) => {
+            state.listFavourites = [action.payload, ...state.listFavourites];
+        }
+
+    }
+});
+
+export const {
+    setUser,
+    setListFavourites,
+    addFavourites,
+    removeFavourites
+} = userSlice.actions;
+
+export default userSlice.reducer;
