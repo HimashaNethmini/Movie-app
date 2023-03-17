3960060774c112c3d6a31e0d74bd9e14ee6c4405
HimashaNethmini <himashanethmini05@gmail.com>
2023-03-17 10:58:15 +0530

    Updated Redux Features

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit"
-import userSlice from "./features/userSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import appStateSlice from "./features/appStateSlice";
+import authModalSlice from "./features/authModalSlice";
+import globalLoadingSlice from "./features/globalLoadingSlice";
+import themeModeSlice from "./features/themeModeSlice";
+import userSlice from "./features/userSlice";
 
 const store = configureStore({
-    reducer: {
-        user: userSlice
-    }
-})
+  reducer: {
+    user: userSlice,
+    themeMode: themeModeSlice,
+    authModal: authModalSlice,
+    globalLoading: globalLoadingSlice,
+    appState: appStateSlice
+  }
+});
 
 export default store;
