59d9e0cedb57d4e5d13a87f9e524dca6c23d0a21
HimashaNethmini <himashanethmini05@gmail.com>
2023-03-17 12:25:14 +0530

    Updated backend files

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 mongoose.connect(process.env.MONGODB_URL).then(()=> {
     console.log("Database is successfully connected")
     server.listen(port, () => {
-        console.log('Server is listening on port ${port}')
+        console.log(`Server is listening on port ${port}`);
     });
 }).catch((err) => {
     console.log({err});
--- server/src/tmdb/tmdb.endpoints.js
+++ server/src/tmdb/tmdb.endpoints.js
@@ -1,33 +1,33 @@
 const tmdbEndpoints = {
     mediaList: ({ mediaType, mediaCategory, page}) => tmdbConfig.getUrl(
-        '${mediaType}/${mediaCategory}' , page
+        `${mediaType}/${mediaCategory}` , page
     ),
     mediaDetail:({mediaType,mediaId}) => tmdbConfig.getUrl(
-        '${mediaType}/${mediaId}'
+        `${mediaType}/${mediaId}`
     ),
     mediaGenres: ({mediaType}) => tmdbConfig.getUrl(
-        'genre/${mediaType}/list'
+        `genre/${mediaType}/list`
     ),
     mediaCredits:({mediaType,mediaId}) => tmdbConfig.getUrl(
-        '${mediaType}/${mediaId}/credits'
+        `${mediaType}/${mediaId}/credits`
     ),
     mediaVideos:({mediaType,mediaId}) => tmdbConfig.getUrl(
-        '${mediaType}/${mediaId}/videos'
+        `${mediaType}/${mediaId}/videos`
     ),
     mediaRecommend:({mediaType,mediaId}) => tmdbConfig.getUrl(
-        '${mediaType}/${mediaId}/recommendations'
+        `${mediaType}/${mediaId}/recommendations`
     ),
     mediaImages:({mediaType,mediaId}) => tmdbConfig.getUrl(
-        '${mediaType}/${mediaId}/images'
+        `${mediaType}/${mediaId}/images`
     ),
     mediaSearch:({mediaType,query,page}) => tmdbConfig.getUrl(
-        'search/${mediaType}',{query,page}
+        `search/${mediaType}`,{query,page}
     ),
     personDetail: ({personId}) => tmdbConfig.getUrl(
-        'person/${personId}'
+        `person/${personId}`
     ),
     personMedias: ({personId}) => tmdbConfig.getUrl(
-        'person/${personId}/combined_credits'
+        `person/${personId}/combined_credits`
     ),
 };
 
